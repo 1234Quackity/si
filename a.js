597036a934f574488507a33cebe15200e7580780
1234Quackity <126984511+1234Quackity@users.noreply.github.com>
2023-09-09 14:00:00 -0600

    Add files via upload

---
+++ a.js
@@ -0,0 +1,12 @@
+ae=[];
+function i() {
+    var s=document.getElementById("e").value;
+    var u=document.getElementById("s").value;
+    var ñ=document.getElementById("m").value;
+    var o=document.getElementById("l").value;
+    ae.push(s);
+    ae.push(u);
+    ae.push(ñ);
+    ae.push(o);
+    document.getElementById("a").innerHTML=ae;
+}
